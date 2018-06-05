2f336fc5ee41272592ee3a7b3ac6c44d4422bd10
kxace <kuangxiao.ace@gmail.com>
2018-06-04 23:27:28 -0400

    add two numbers

---
+++ src/Algorithms/AddTwoNumbers.java
@@ -0,0 +1,61 @@
+package Algorithms;
+
+/**
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+ You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ Example
+
+ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ Output: 7 -> 0 -> 8
+ Explanation: 342 + 465 = 807.
+ */
+public class AddTwoNumbers {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        int addOne = 0;
+
+        while (l1 != null || l2 != null || addOne != 0) {
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            int sum = val1 + val2 + addOne;
+            head.next = new ListNode(sum % 10);
+            head = head.next;
+            addOne = sum / 10;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        return dummy.next;
+    }
+
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public static String print(ListNode l) {
+            StringBuilder sb = new StringBuilder();
+            while (l != null) {
+                sb.append(l.val);
+                l = l.next;
+            }
+            return sb.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        ListNode l2 = new ListNode(5);
+        l1.next = new ListNode(4);
+        l2.next = new ListNode(6);
+        System.out.println(ListNode.print(addTwoNumbers(l1, l2)));
+    }
+}
